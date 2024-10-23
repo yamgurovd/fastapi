963d819c7aca7ae09926638fe24671112cfbf8a5
dayan <yamgurovd@gmail.com>
2024-10-24 01:07:09 +0300

    Изменил методы и добавил библиотек новых

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 from fastapi import FastAPI
-from starlette.staticfiles import StaticFiles
+from src.fake_users import fake_users
 
 app = FastAPI(
     title="test app"
 )
-
-fake_users = [
-    {"id": 0, "first_name": "Иванов", "last_name": "Иван", "patronymic": "Иванович", "age": 18,
-     "position": "Комплектовщик", "company": "ООО Радуга"},
-    {"id": 1, "first_name": "Петров", "last_name": "Андрей", "patronymic": "Иванович", "age": 35,
-     "position": "Руководитель", "company": "ООО Радуга"},
-    {"id": 2, "first_name": "Андреев", "last_name": "Георгий", "patronymic": "Степанович", "age": 24,
-     "position": "Кладовщик", "company": "ООО Радуга"},
-    {"id": 3, "first_name": "Макарова", "last_name": "Лидия", "patronymic": "Сергеевна", "age": 20,
-     "position": "Оператор", "company": "ООО Радуга"},
-    {"id": 4, "first_name": "Андреев", "last_name": "Георгий", "patronymic": "Степанович", "age": 24,
-     "position": "Кладовщик", "company": "ООО Радуга"},
-    {"id": 5, "first_name": "Сергеева", "last_name": "Ольга", "patronymic": "Петровна", "age": 50,
-     "position": "Кассир", "company": "ООО Радуга"},
-]
 
 
 @app.get("/")
@@ -48,7 +33,7 @@ def add_new_user(user_id: int,
     :param company: Название компании
     :return: Создание нового пользователя
     """
-    new_user = {"user_id": user_id, "first_name": first_name, "last_name": last_name, "patronymic": patronymic,
+    new_user = {"id": user_id, "first_name": first_name, "last_name": last_name, "patronymic": patronymic,
                 "age": age, "position": position, "company": company}
 
     fake_users.append(new_user)
@@ -66,36 +51,27 @@ def update_user_info(user_id: int,
                      company: str):
     """
     :param user_id: id пользователя
-    :param first_name: Имя пользователя
+    :param first_name: Имя
     :param last_name: Фамилия
     :param patronymic: Отчество
     :param age: Возраст
     :param position: Должность
     :param company: Название организации
-    :return:
+    :return: Изменение данных пользователя
     """
-    user = fake_users[user_id]
-    user["first_name"] = first_name
-    user["last_name"] = last_name
-    user["patronymic"] = patronymic
-    user["age"] = age
-    user["position"] = position
-    user["company"] = company
-
-    fake_users[user_id].update(
-        {"id": user_id, "first_name": first_name, "last_name": last_name, "patronymic": patronymic, "age": age,
-         "position": position, "company": company})
-
-    return fake_users[user_id]
+    for user in fake_users:
+        # print(user)
+        if user["id"] == user_id:
+            user.update({
+                "first_name": first_name,
+                "last_name": last_name,
+                "patronymic": patronymic,
+                "age": age,
+                "position": position,
+                "company": company}, )
+            return user
 
 
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    """
-    :param user_id: id пользователя
-    :return: Удаление записи пользователя
-    """
-    return fake_users.pop(user_id)
 @app.get("/users")
 def get_users():
     """
@@ -105,9 +81,25 @@ def get_users():
 
 
 @app.get("/users/{user_id}")
-def get_user_id(user_id: int):
+def user_info(user_id: int):
     """
-    :param user_id: Ввод id пользователя
-    :return: Данные о пользователе
+    :param user_id: id пользователя
+    :return: Информацию о пользователе
     """
-    return fake_users[user_id]
+    for user in fake_users:
+        # print(user)
+        if user["id"] == user_id:
+            return user
+
+
+@app.delete("/users/{user_id}")
+def delete_user_id(user_id: int):
+    """
+        :param user_id: id пользователя
+        :return: Удаление записи пользователя
+        """
+    for user in fake_users:
+        if user["id"] == user_id:
+            fake_users.remove(user)
+
+        return {"id": user_id}
