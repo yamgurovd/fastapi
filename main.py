f0532f569d7ed42d42ea3d6e64c23c7ed2baec73
d <yamgurovd@gmail.com>
2025-01-07 00:50:40 +0300

    2.11 Решение PUT и PATCH ручки

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import uvicorn
 app = FastAPI()
 
 hotels = [
-    {"id": 1, "title": "Sochi"},
-    {"id": 2, "title": "Дубай"},
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
 ]
 
 
@@ -33,6 +33,38 @@ def create_hotel(
         "id": hotels[-1]["id"] + 1,
         "title": title
     })
+    return {"status": "OK"}
+
+
+@app.put("/hotels/{hotel_id}")
+def edit_hotel(
+        hotel_id: int,
+        title: str = Body(),
+        name: str = Body(),
+):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    hotel["title"] = title
+    hotel["name"] = name
+    return {"status": "OK"}
+
+
+@app.patch(
+    "/hotels/{hotel_id}",
+    summary="Частичное обновление данных об отеле",
+    description="<h1>Тут мы частично обновляем данные об отеле: можно отправить name, а можно title</h1>",
+)
+def partially_edit_hotel(
+        hotel_id: int,
+        title: str | None = Body(None),
+        name: str | None = Body(None),
+):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    if title:
+        hotel["title"] = title
+    if name:
+        hotel["name"] = name
     return {"status": "OK"}
 
 
