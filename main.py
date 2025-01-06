ba31d03c7127735f38c939e1f9aad218c24f68f9
d <yamgurovd@gmail.com>
2025-01-07 00:40:31 +0300

    2.7 Первая ручка получение отелей

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import uvicorn
 
 app = FastAPI()
 
-
-@app.get("/")
-def func():
-    return "Hello World!!!!!!!!!!"
+hotels = [
+    {"id": 1, "title": "Sochi"},
+    {"id": 2, "title": "Дубай"},
+]
+@app.get("/hotels")
+def get_hotels(
+        id: int | None = Query(None, description="Айдишник"),
+        title: str | None = Query(None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
 
 
 if __name__ == "__main__":
