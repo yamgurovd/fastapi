a69cbbb68737f945e07fda1bf16449cf2fe4f62d
d <yamgurovd@gmail.com>
2025-01-07 00:52:28 +0300

    2.4 Синхронный VS Асинхронный FastAPI — наглядное сравнение

---
+++ course_helpers/2 Осваиваем FastAPI/4 Синхронный VS Асинхронный FastAPI — наглядное сравнение.py
@@ -0,0 +1,27 @@
+import asyncio
+import time
+import threading
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(docs_url=None)
+
+
+@app.get("/sync/{id}")
+def sync_func(id: int):
+    print(f"sync. Потоков: {threading.active_count()}")
+    print(f"sync. Начал {id}: {time.time():.2f}")
+    time.sleep(3)
+    print(f"sync. Закончил {id}: {time.time():.2f}")
+
+
+@app.get("/async/{id}")
+async def async_func(id: int):
+    print(f"async. Потоков: {threading.active_count()}")
+    print(f"async. Начал {id}: {time.time():.2f}")
+    await asyncio.sleep(3)
+    print(f"async. Закончил {id}: {time.time():.2f}")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
