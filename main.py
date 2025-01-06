08828a8e258dbb7e101c7f2ba41420c774b04d92
d <yamgurovd@gmail.com>
2025-01-07 00:45:14 +0300

    2.9 Первая POST  ручка добавление отелей

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Body
 import uvicorn
 
 app = FastAPI()
@@ -7,6 +7,8 @@ hotels = [
     {"id": 1, "title": "Sochi"},
     {"id": 2, "title": "Дубай"},
 ]
+
+
 @app.get("/hotels")
 def get_hotels(
         id: int | None = Query(None, description="Айдишник"),
@@ -22,11 +24,24 @@ def get_hotels(
     return hotels_
 
 
+@app.post("/hotels")
+def create_hotel(
+        title: str = Body(embed=True),
+):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title
+    })
+    return {"status": "OK"}
+
+
 @app.delete("/hotels/{hotel_id}")
 def delete_hotel(hotel_id: int):
     global hotels
     hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
     return {"status": "OK"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
