8ae3291bf85b7523d2d8c8d0c93c6ba878d5d77b
d <yamgurovd@gmail.com>
2025-01-07 20:24:31 +0300

    3.5 Как работают транзакции в базе

--- src/database.py
+++ src/database.py
@@ -3,4 +3,5 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from src.config import settings
 
 engine = create_async_engine(settings.DB_URL)
+
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
