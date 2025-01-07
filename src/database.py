777ad664ed6cae2985918e3456800ef144778858
d <yamgurovd@gmail.com>
2025-01-07 21:33:18 +0300

    3.6 Создание модели отелей через SQLAlchemy

--- src/database.py
+++ src/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
 engine = create_async_engine(settings.DB_URL)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
