79be0fc1d4aa1e15d03f333b262b07fce99e0ddb
d <yamgurovd@gmail.com>
2025-01-07 23:14:42 +0300

    3.11 Добавление отелей через SQLAlchemy

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,6 +1,9 @@
-from fastapi import Query, APIRouter
+from fastapi import Query, APIRouter, Body
 
+from sqlalchemy import insert
 from src.api.dependencies import PaginationDep
+from src.database import async_session_maker
+from src.models.hotels import HotelsOrm
 from src.schemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
@@ -30,13 +33,27 @@ def get_hotels(
 
 
 @router.post("")
-def create_hotel(hotel_data: Hotel):
-    global hotels
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": hotel_data.title,
-        "name": hotel_data.name,
-    })
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+    "1": {
+        "summary": "Сочи",
+        "value": {
+            "title": "Отель Сочи 5 звезд у моря",
+            "location": "ул. Моря, 1",
+        }
+    },
+    "2": {
+        "summary": "Дубай",
+        "value": {
+            "title": "Отель Дубай У фонтана",
+            "location": "ул. Шейха, 2",
+        }
+    }
+})
+):
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
     return {"status": "OK"}
 
 
