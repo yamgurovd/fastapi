8dd5dd618ad02f6e7b5421a167ea10902f6afbf2
d <yamgurovd@gmail.com>
2025-01-08 21:38:15 +0300

    3.15 Решение фильтрация в подстроке

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,6 +1,6 @@
 from fastapi import Query, APIRouter, Body
 
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, func
 
 from src.api.dependencies import PaginationDep
 from src.database import async_session_maker, engine
@@ -13,29 +13,27 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 @router.get("")
 async def get_hotels(
         pagination: PaginationDep,
-        id: int | None = Query(None, description="Айдишник"),
+        location: str | None = Query(None, description="Локация"),
         title: str | None = Query(None, description="Название отеля"),
 ):
-
     per_page = pagination.per_page or 5
+
     async with async_session_maker() as session:
         query = select(HotelsOrm)
 
-        if id:
-            query = query.filter_by(id=id)
-
+        if location:
+            query = query.filter(func.lower(HotelsOrm.location).like(f"%{location.strip().lower()}%"))
         if title:
-            query = query.filter_by(title=title)
-
+            query = query.filter(func.lower(HotelsOrm.title).like(f"%{title.strip().lower()}%"))
         query = (
             query
             .limit(per_page)
             .offset(per_page * (pagination.page - 1))
         )
+        print(query.compile(compile_kwargs={"literal_binds": True}))
         result = await session.execute(query)
         hotels = result.scalars().all()
         print(type(hotels), hotels)
-
 
         return hotels
 
