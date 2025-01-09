f2c20365fd01711d610e231f50b42b3d60cec4ac
d <yamgurovd@gmail.com>
2025-01-09 20:40:41 +0300

    3.18 Решение вставка данных через репозиторий

--- src/repasitories/base.py
+++ src/repasitories/base.py
@@ -1,4 +1,5 @@
-from sqlalchemy import select
+from pydantic import BaseModel
+from sqlalchemy import select, insert
 
 
 class BaseRepository:
@@ -16,3 +17,8 @@ class BaseRepository:
         query = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(query)
         return result.scalars().one_or_none()
+
+    async def add(self, data: BaseModel):
+        add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        result = await self.session.execute(add_data_stmt)
+        return result.scalars().one()
