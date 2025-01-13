356d4b50b1af88309a9dbb29ea14ff94a2081b53
d <yamgurovd@gmail.com>
2025-01-13 22:04:15 +0300

    4.3 Регистрация пользователей - хеширование паролей

--- src/main.py
+++ src/main.py
@@ -10,8 +10,10 @@ sys.path.append(str(Path(__file__).parent.parent))
 app = FastAPI()
 
 from src.api.hotels import router as router_hotels
+from src.api.auth import router as router_auth
 
+app.include_router(router_auth)
 app.include_router(router_hotels)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0",  reload=True)
