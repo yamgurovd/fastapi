ede059cfbcf152b8dfa97873382c70f2e9baf170
d <yamgurovd@gmail.com>
2025-01-14 18:25:26 +0300

    4.4 Решение Запретить регистрацию нескольких юзеров на одну почту

--- src/models/users.py
+++ src/models/users.py
@@ -6,5 +6,5 @@ from src.database import Base
 class UsersOrm(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(200))
+    email: Mapped[str] = mapped_column(String(200), unique=True)
     hashed_password: Mapped[str] = mapped_column(String(200))
