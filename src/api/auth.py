af1d50ab20f809122d0b09b67468c43a6e03bead
d <yamgurovd@gmail.com>
2025-01-15 23:35:53 +0300

    4.5 Аутентификация - проверка пароля и отправка jwt токена - UPD

--- src/api/auth.py
+++ src/api/auth.py
@@ -29,14 +29,12 @@ def create_access_token(data: dict) -> str:
 
 
 @router.post("/register")
-async def register_user(
-        data: UserRequestAdd,
-):
+async def register_user(data: UserRequestAdd):
     hashed_password = pwd_context.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
-
+        await session.commit()  # Ensure changes are committed
     return {"status": "OK"}
 
 
