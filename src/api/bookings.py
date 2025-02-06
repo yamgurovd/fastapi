6b93b66dcbc5051ec09e39f06977165e88540263
d <yamgurovd@gmail.com>
2025-02-06 23:42:35 +0300

    5.6 Решение ручки на получение и бронирование бронирования

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -5,6 +5,16 @@ from src.schemas.bookings import BookingAddRequest, BookingAdd
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 
+@router.get("")
+async def get_bookings(db: DBDep):
+    return await db.bookings.get_all()
+
+
+@router.get("/me")
+async def get_my_bookings(user_id: UserIdDep, db: DBDep):
+    return await db.bookings.get_filtered(user_id=user_id)
+
+
 @router.post("")
 async def add_booking(
         user_id: UserIdDep,
