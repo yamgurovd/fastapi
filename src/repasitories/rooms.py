46a30a24798a7d09f1c34b5719b41766c878a264
d <yamgurovd@gmail.com>
2025-02-14 00:00:45 +0300

    5.8 5.7 Получение доступных номеров - перевод из SQL в ORM запрос - заключение 2

--- src/repasitories/rooms.py
+++ src/repasitories/rooms.py
@@ -63,13 +63,20 @@ class RoomsRepository(BaseRepository):
             .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
             .cte(name="rooms_left_table")
         )
-        """
-        !select * from rooms_left_table
-        where rooms_left > 0
-        """
-        query = (
-            select(rooms_left_table)
-            .select_from(rooms_left_table)
-            .filter(rooms_left_table.c.rooms_left > 0)
+        rooms_ids_for_hotel = (
+            select(RoomsOrm.id)
+            .select_from(RoomsOrm)
+            .filter_by(hotel_id=hotel_id)
+            .subquery(name="rooms_ids_for_hotel")
         )
-        print(query.compile(bind=engine, compile_kwargs={"literal_binds": True}))
+
+        rooms_ids_to_get = (
+            select(rooms_left_table.c.room_id)
+            .select_from(rooms_left_table)
+            .filter(
+                rooms_left_table.c.rooms_left > 0,
+                rooms_left_table.c.room_id.in_(rooms_ids_for_hotel),
+            )
+        )
+
+        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
