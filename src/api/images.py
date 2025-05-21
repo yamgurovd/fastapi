4f795f75e8042ed18228835d972d676d7adddba2
d <yamgurovd@gmail.com>
2025-05-21 22:45:38 +0300

    6.4 Фоновые задачи в FastApi

--- src/api/images.py
+++ src/api/images.py
@@ -1,6 +1,6 @@
 import shutil
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, BackgroundTasks
 
 from src.tasks.tasks import resize_image
 
@@ -8,9 +8,10 @@ router = APIRouter(prefix="/images", tags=["Изображения отелей"
 
 
 @router.post("")
-def upload_image(file: UploadFile):
+def upload_image(file: UploadFile, background_tasks: BackgroundTasks):
     image_path = f"src/static/images/{file.filename}"
     with open(image_path, "wb+") as new_file:
         shutil.copyfileobj(file.file, new_file)
 
-    resize_image.delay(image_path)
+    # resize_image.delay(image_path)
+    background_tasks.add_task(resize_image, image_path)
