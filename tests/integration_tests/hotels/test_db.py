265c542f1caa295e9d89a911cbb81af8cb2141db
d <yamgurovd@gmail.com>
2025-05-22 20:37:03 +0300

    7.2 Первый тест на базу данных

---
+++ tests/integration_tests/hotels/test_db.py
@@ -0,0 +1,10 @@
+from src.database import async_session_maker
+from src.schemas.hotels import HotelAdd
+from src.utils.db_manager import DBManager
+
+
+async def test_add_hotel():
+    hotel_data = HotelAdd(title="Hotel 5 stars", location="Сочи")
+    async with DBManager(session_factory=async_session_maker) as db:
+        new_hotel_data = await db.hotels.add(hotel_data)
+        print(f"{new_hotel_data=}")
