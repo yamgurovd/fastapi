8ee5316cbae75fc50ca931f0727f14407e67b4d9
d <yamgurovd@gmail.com>
2025-05-22 20:58:03 +0300

    7.2 Создание тестовой базы данных(2)

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,12 @@ from src.models import *
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main():
-    print("Я ФИКСТУРА")
+def check_test_mode():
     assert settings.MODE == "TEST"
 
+
+@pytest.fixture(scope="session", autouse=True)
+async def async_main(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
