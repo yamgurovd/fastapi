d23cb754fdc3b212053b7c01be5cb0ca5ecf772d
d <yamgurovd@gmail.com>
2025-05-26 21:32:25 +0300

    7.11 Параметризация тестов

--- src/repasitories/bookings.py
+++ src/repasitories/bookings.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from fastapi import HTTPException
 from sqlalchemy import select
 
 from src.models.bookings import BookingsOrm
@@ -35,4 +36,4 @@ class BookingsRepository(BaseRepository):
             new_booking = await self.add(data)
             return new_booking
         else:
-            raise Exception
+            raise HTTPException(500)
